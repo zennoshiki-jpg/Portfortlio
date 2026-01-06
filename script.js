f7c8f91e1edb78259c80aaceb326cb9e033b2b7c
zennoshiki-jpg <zennoshiki@gmail.com>
2026-01-07 00:47:35 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Contact form alert
+document.querySelector(".contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+});
